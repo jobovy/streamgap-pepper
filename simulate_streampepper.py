3287de34dc1a1606aef1a6315d96705a7ce4b1f5
Jo Bovy <bovy@astro.utoronto.ca>
2016-02-05 15:50:16 -0500

    Allow a factor to be applied to the rs(M) relation

--- simulate_streampepper.py
+++ simulate_streampepper.py
@@ -29,6 +29,8 @@ def get_options():
                       help="Mass or mass range to consider; given as log10(mass)")
     parser.add_option("--timescdm",dest='timescdm',default=1.,
                       help="Use a rate that is timescdm times the CDM prediction")
+    parser.add_option("--rsfac",dest='rsfac',default=1.,
+                      help="Use a r_s(M) relation that is a factor of rsfac different from the fiducial one")
     parser.add_option("--plummer",action="store_true", 
                       dest="plummer",default=False,
                       help="If set, use a Plummer DM profile rather than Hernquist")
@@ -60,14 +62,15 @@ def parse_mass(mass):
 # Functions to sample
 def nsubhalo(m):
     return 0.3*(10.**6.5/m)
-def rs(m,plummer=False):
+def rs(m,plummer=False,rsfac=1.):
     if plummer:
-        return 1.62/R0*(m/10.**8.)**0.5
+        return 1.62*rsfac/R0*(m/10.**8.)**0.5
     else:
-        return 1.05/R0*(m/10.**8.)**0.5
-def dNencdm(sdf_pepper,m,Xrs=3.,plummer=False):
+        return 1.05*rsfac/R0*(m/10.**8.)**0.5
+def dNencdm(sdf_pepper,m,Xrs=3.,plummer=False,rsfac=1.):
     return sdf_pepper.subhalo_encounters(\
-        sigma=120./220.,nsubhalo=nsubhalo(m),bmax=Xrs*rs(m,plummer=plummer))
+        sigma=120./220.,nsubhalo=nsubhalo(m),
+        bmax=Xrs*rs(m,plummer=plummer,rsfac=rsfac))
 
 # Function to run the simulations
 def run_simulations(sdf_pepper,sdf_smooth,options):
@@ -109,7 +112,8 @@ def run_simulations(sdf_pepper,sdf_smooth,options):
             /bovy_conversion.mass_in_1010msol(V0,R0)
         rate= options.timescdm*dNencdm(sdf_pepper,
                                        10.**massrange[0],Xrs=options.Xrs,
-                                       plummer=options.plummer)
+                                       plummer=options.plummer,
+                                       rsfac=options.rsfac)
     elif len(massrange) == 2:
         # Sample from power-law
         sample_GM= lambda: (10.**-(massrange[0]/2.)\
@@ -120,11 +124,11 @@ def run_simulations(sdf_pepper,sdf_smooth,options):
         rate_range= numpy.arange(massrange[0]+0.5,massrange[1]+0.5,1)
         rate= options.timescdm\
             *numpy.sum([dNencdm(sdf_pepper,10.**r,Xrs=options.Xrs,
-                                plummer=options.plummer)
+                                plummer=options.plummer,rsfac=options.rsfac)
                         for r in rate_range])
     print "Using an overall rate of %f" % rate
     sample_rs= lambda x: rs(x*bovy_conversion.mass_in_1010msol(V0,R0)*10.**10.,
-                            plummer=options.plummer)
+                            plummer=options.plummer,rsfac=options.rsfac)
     # Simulate
     start= time.time()
     while time.time() < (start+options.dt*60.):

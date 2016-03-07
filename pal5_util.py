bb47eff48e26b3da2eed1594980770dc0a3ecce9
Jo Bovy <bovy@astro.utoronto.ca>
2016-03-07 18:33:12 -0500

    Add pal5 model

--- pal5_util.py
+++ pal5_util.py
@@ -1,5 +1,10 @@
 import numpy
-from galpy.util import bovy_coords
+from galpy.potential import MWPotential2014
+from galpy.actionAngle import actionAngleIsochroneApprox
+from galpy.orbit import Orbit
+from galpy.df import streamdf
+from galpy.util import bovy_conversion, bovy_coords
+from gd1_util import R0, V0
 # Coordinate transformation routines
 _RAPAL5= 229.018/180.*numpy.pi
 _DECPAL5= -0.124/180.*numpy.pi
@@ -20,3 +25,45 @@ def radec_to_pal5xieta(ra,dec,degree=False):
     phi1= numpy.arctan2(phiXYZ[1],phiXYZ[0])
     return numpy.array([phi1,phi2]).T
 
+def setup_pal5model(leading=False,
+                    timpact=None,
+                    hernquist=True,
+                    age=5.,
+                    singleImpact=False,
+                    **kwargs):
+    obs= Orbit([229.018,-0.124,23.2,-2.296,-2.257,-58.7],
+               radec=True,ro=R0,vo=V0,
+               solarmotion=[-11.1,24.,7.25])
+    aAI= actionAngleIsochroneApprox(pot=MWPotential2014,b=0.8)
+    sigv= 0.5*(5./age) #km/s, adjust for diff. age
+    if timpact is None:
+        sdf= streamdf(sigv/V0,progenitor=obs,
+                      pot=MWPotential2014,aA=aAI,
+                      leading=leading,nTrackChunks=11,
+                      tdisrupt=age/bovy_conversion.time_in_Gyr(V0,R0),
+                      Rnorm=R0,Vnorm=V0,R0=R0,
+                      vsun=[-11.1,V0+24.,7.25],
+                      custom_transform=_TPAL5)
+    elif singleImpact:
+        sdf= streamdf(sigv/V0,progenitor=obs,
+                      pot=MWPotential2014,aA=aAI,
+                      leading=leading,nTrackChunks=11,
+                      tdisrupt=age/bovy_conversion.time_in_Gyr(V0,R0),
+                      Rnorm=R0,Vnorm=V0,R0=R0,
+                      vsun=[-11.1,V0+24.,7.25],
+                      custom_transform=_TPAL5,
+                      timpact=timpact,
+                      spline_order=3,
+                      hernquist=hernquist,**kwargs)
+    else:
+        sdf= streamdf(sigv/V0,progenitor=obs,
+                      pot=MWPotential2014,aA=aAI,
+                      leading=leading,nTrackChunks=101,
+                      tdisrupt=age/bovy_conversion.time_in_Gyr(V0,R0),
+                      Rnorm=R0,Vnorm=V0,R0=R0,
+                      vsun=[-11.1,V0+24.,7.25],
+                      custom_transform=_TPAL5,
+                      timpact=timpact,
+                      spline_order=1,
+                      hernquist=hernquist)
+    return sdf
